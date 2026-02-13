a626285ea3f6892e23cb6964344cd3745010fd01
Li yi dun <yidundevelopwa@hotmail.com>
2026-02-12 22:35:31 -0800

    Add structured data (Person schema)

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Milton Diaz",
+              alternateName: "Li Yi Dun",
+              url: "https://diyidun.github.io/",
+              image: "https://diyidun.github.io/me.jpeg",
+              jobTitle: "Full-Stack Web Developer",
+              knowsAbout: [
+                "React",
+                "Tailwind CSS",
+                "JavaScript",
+                "Node.js",
+                "Express.js",
+                "MySQL",
+                "REST APIs",
+                "SaaS Development",
+                "Multi-tenant Architecture",
+                "PayPal Integration",
+                "Nginx",
+                "Linux Server Deployment",
+              ],
+              sameAs: [
+                "https://github.com/diyidun",
+                "https://www.linkedin.com/in/milton-diaz-li-yi-dun-583597142",
+              ],
+            }),
+          }}
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
